cf6c7991eeea001c454cadcd6662ffdf6205dc4e
hurrytears <hurrytears@gmail.com>
2020-11-27 15:34:33 +0800

    first commit

---
+++ src/main/java/com/apache/netty/secondexample/MyServer.java
@@ -0,0 +1,27 @@
+package com.apache.netty.secondexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boss, worker).channel(NioServerSocketChannel.class)
+                    .childHandler(new MyServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(9988).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
