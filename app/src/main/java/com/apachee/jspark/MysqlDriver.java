c2a665607e8d006f07a5de019cf4191bf41a16e9
mixree <75112956+hurrytears@users.noreply.github.com>
2021-05-13 15:25:54 +0800

    删除

--- app/src/main/java/com/apachee/jspark/MysqlDriver.java
+++
@@ -1,8 +0,0 @@
-package com.apachee.jspark;
-
-public class MysqlDriver {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- flink/src/main/scala/com/practice/WordCount.scala
+++
@@ -1,9 +0,0 @@
-package com.practice
-
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-
-object WordCount {
-    def main(args: Array[String]): Unit = {
-        val env = StreamExecutionEnvironment.getExecutionEnvironment
-    }
-}
