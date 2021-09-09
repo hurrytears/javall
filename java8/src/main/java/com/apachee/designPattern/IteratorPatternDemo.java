463d1626b10b806e6406cf0ef3c2237c4a07c8e6
sosog <hurrytears@gmail.com>
2021-09-09 22:34:12 +0800

    迭代器模式，应该迭代器都是这么实现的，只是多了个内部变量的非空判断

---
+++ java8/src/main/java/com/apachee/designPattern/IteratorPatternDemo.java
@@ -0,0 +1,56 @@
+package com.apachee.designPattern;
+
+
+// 迭代器模式
+
+interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
+
+interface Container {
+    public Iterator getIterator();
+}
+
+class NameRepository implements Container {
+    public String[] names = {"Robert" , "John" ,"Julie" , "Lora"};
+
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+
+        // 这个变量没办法写到interface里
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            if(index < names.length){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            if(this.hasNext()){
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
+
+public class IteratorPatternDemo {
+
+    public static void main(String[] args) {
+        NameRepository namesRepository = new NameRepository();
+
+        for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+            String name = (String)iter.next();
+            System.out.println("Name : " + name);
+        }
+    }
+}
