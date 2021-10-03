b09e342aa558ecdbd90a9cfaf9d4425db90d0181
sosog <hurrytears@gmail.com>
2021-10-03 16:49:32 +0800

    jwt begion

---
+++ app/src/main/java/com/apachee/jwt/Demo.java
@@ -0,0 +1,33 @@
+package com.apachee.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+
+public class Demo {
+    public static void main(String[] args) {
+        String token;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            token = JWT.create()
+                    .withIssuer("auth0")
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            //Invalid Signing configuration / Couldn't convert Claims.
+        }
+
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCJ9.nAbIJTDMFc7yUa5MhvcP03nJPyCPzZtQcGEp-zWfOkEE";
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("auth0")
+                    .build(); //Reusable verifier instance
+            DecodedJWT jwt = verifier.verify(token);
+        } catch (JWTVerificationException exception){
+            //Invalid signature/claims
+        }
+    }
+}
