812d4fe0205216ee70d5865a089d467e96fa235b
sosog <hurrytears@gmail.com>
2021-10-06 17:20:09 +0800

    可以实现用cookie提取_csrf参数上传文件

--- spring/src/main/java/com/apachee/spring/config/SecurityConfig.java
+++ spring/src/main/java/com/apachee/spring/config/SecurityConfig.java
@@ -12,10 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -23,23 +24,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // 这里配置的也只是get方式提交的请求不需要拦截
                 .antMatchers("/css/**", "/js/**", "/").permitAll()
-                .antMatchers("/users/**").hasRole("ADMIN")
+                .antMatchers("/users/**").hasRole("user")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                // 前后端分离的csrf cookie 配置，允许js提取cookie
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .logout().permitAll();
     }
 
     // 配置security 登录用户，这个和application.properties 文件配置冲突
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("admin").password(encoder.encode("123456")).roles("SuperAdmin")
+                .withUser("admin").password(encoder.encode("123456")).roles("admin")
                 .and()
-                .withUser("sosog").password(encoder.encode("123456")).roles("Admin")
+                .withUser("sosog").password(encoder.encode("123456")).roles("dep")
                 .and()
-                .passwordEncoder(new BCryptPasswordEncoder());
+                .passwordEncoder(encoder);
     }
 }
