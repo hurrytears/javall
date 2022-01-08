20043c48645974fdbc482eae6af2948d432315cb
sosog_zhang <hurrytears@gmail.com>
2022-01-09 04:44:55 +0800

    spark 官方手册Rdd

--- spark/src/main/java/com/apachee/core/RddProgrammingGuide.java
+++ spark/src/main/java/com/apachee/core/RddProgrammingGuide.java
@@ -3,12 +3,13 @@ package com.apachee.core;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.storage.StorageLevel;
+import org.apache.spark.util.LongAccumulator;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
-
 public class RddProgrammingGuide {
 
     private static SparkConf conf = new SparkConf().setAppName(RddProgrammingGuide.class.getName()).setMaster("local");
@@ -18,7 +19,7 @@ public class RddProgrammingGuide {
     public void parallelizedCollections(){
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
         JavaRDD<Integer> distData = sc.parallelize(data, 5);
-        distData.reduce((a, b) -> a + b);
+        distData.reduce(Integer::sum);
         distData.persist(StorageLevel.MEMORY_ONLY());
 
         // 本地文件必须出现在所有的workder节点上
@@ -37,12 +38,28 @@ public class RddProgrammingGuide {
          * rdd.saveAsObjectFile() 存储为文件
          * sc.objectFile()
          */
-        distFile.map(line -> line.length()).reduce((a, b) -> a + b);
+        distFile.map(String::length).reduce(Integer::sum);
     }
 
     // Understanding closures
     // Transformations
     // Actions
+
+    // Shuffle operations
+    // Performance Impact
+    // persist() and  unpersist()
+
+    @Test
+    public void broadCastVariable(){
+        Broadcast<Integer> integerBroadcast = sc.broadcast(1);
+        System.out.println(integerBroadcast.value());
+        integerBroadcast.unpersist();
+        LongAccumulator accum = sc.sc().longAccumulator();
+
+        sc.parallelize(Arrays.asList(1,2,3,4,5)).foreach(accum::add);
+
+        System.out.println(accum.value());
+    }
 
 
 }
